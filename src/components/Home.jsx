20a01c2f53421e0825578567828ded948e06466c
Sridhar3145 <sridhar314507@gmail.com>
2026-06-12 15:56:29 +0530

    image updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import nannarisarbath from "../assets/syrup-sarbath.webp";
 import nannarisarbath from "../assets/selvam-sarbath-syrup.webp";
 
 
